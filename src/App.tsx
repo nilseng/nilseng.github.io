67d574e3dd61e56ef029342f6ed6354a3d41226c
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2020-10-25 17:40:52 +0100

    using three and the 1. and 2. equations of motion to animate a falling and rolling ball

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,19 @@ import history from "./utils/history";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Blog from "./components/Blog/Blog";
+import BackgroundAnimation from "./components/BackgroundAnimation";
 
 const App = () => {
   return (
     <>
       <Router history={history}>
         <NavBar />
-        <Container className="p-4" style={{ minHeight: "calc(100vh - 259px)" }}>
+        <BackgroundAnimation />
+        <Container className="p-4 appContainer">
           <Switch>
             <Route path="/blog" component={Blog} />
           </Switch>
         </Container>
-
         <Footer />
       </Router>
     </>
---
+++ src/components/BackgroundAnimation.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+let renderer: any,
+  scene: any,
+  camera: any,
+  sphere: THREE.Mesh,
+  v0y: number = 0;
+
+const init = (mainEl: React.RefObject<HTMLDivElement>) => {
+  camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight
+  );
+  camera.position.z = 10;
+
+  scene = new THREE.Scene();
+
+  //Create a plane that receives shadows (but does not cast them)
+  var planeGeometry = new THREE.PlaneBufferGeometry(5, 5, 32, 32);
+  var planeMaterial = new THREE.MeshStandardMaterial({ color: "#f8f9fa" });
+  var floor = new THREE.Mesh(planeGeometry, planeMaterial);
+  floor.receiveShadow = true;
+  floor.rotateX(-Math.PI / 2);
+  floor.position.set(0, -2, 0);
+  scene.add(floor);
+
+  // Adding a sphere
+  const sphereGeometry = new THREE.SphereBufferGeometry(0.5, 32, 32);
+  const sphereMaterial = new THREE.MeshStandardMaterial({ color: "#faf8f9" });
+
+  sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.castShadow = true;
+  sphere.position.set(-4, 4, 0);
+  scene.add(sphere);
+
+  // Adding light
+  const pointLight = new THREE.PointLight(0xffffff, 2);
+  pointLight.position.set(1, 1, 10);
+  pointLight.castShadow = true;
+  scene.add(pointLight);
+
+  //Set up shadow properties for the light
+  pointLight.shadow.mapSize.width = 5000;
+  pointLight.shadow.mapSize.height = 5000;
+  pointLight.shadow.camera.near = 0.5;
+  pointLight.shadow.camera.far = 500;
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.render(scene, camera);
+  mainEl.current?.appendChild(renderer.domElement);
+};
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  sphere.position.x += 0.05;
+  sphere.position.y +=
+    sphere.position.y >= -1.5 || sphere.position.x > 2.5
+      ? v0y * (1 / 60) + (1 / 2) * -9.81 * Math.pow(1 / 60, 2)
+      : 0;
+  v0y =
+    sphere.position.y >= -1.5 || sphere.position.x > 2.5
+      ? v0y - 9.81 * (1 / 60)
+      : 0;
+  if (sphere.position.y < -4) {
+    sphere.position.y = 4;
+    sphere.position.x = -4;
+    v0y = 0;
+  }
+  if (renderer) renderer.render(scene, camera);
+};
+
+const BackgroundAnimation = () => {
+  const mainEl = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    init(mainEl);
+    animate();
+  });
+  return (
+    <>
+      <div
+        ref={mainEl}
+        className="h-100 w-100"
+        style={{ position: "absolute", top: 0, zIndex: -1 }}
+      ></div>
+    </>
+  );
+};
+
+export default BackgroundAnimation;
