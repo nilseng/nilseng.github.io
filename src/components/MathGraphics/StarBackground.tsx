b67ec84046b844738a2627403fb63aef0361a881
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2021-03-29 23:00:47 +0200

    adding 10 times more stars

--- src/components/MathGraphics/StarBackground.tsx
+++ src/components/MathGraphics/StarBackground.tsx
@@ -7,22 +7,24 @@ const StarBackground = () => {
       viewBox="0 0 100 100"
       style={{ zIndex: -1 }}
     >
-      {new Array(1000).fill(1).map((_, i) => (
-        <circle
-          key={i}
-          cx={100 * Math.random()}
-          cy={100 * Math.random()}
-          fill="#f8f9fa"
-        >
-          <animate
-            attributeName="r"
-            values={`0;${0.1 * Math.random()};0`}
-            begin={`${Math.random()}s`}
-            dur={`${2 + 4 * Math.random()}s`}
-            repeatCount="indefinite"
-          />
-        </circle>
-      ))}
+      {new Array(10).fill(1).map((_, i) =>
+        new Array(1000).fill(1).map((_, j) => (
+          <circle
+            key={i * 1000 + j}
+            cx={100 * Math.random()}
+            cy={100 * Math.random()}
+            fill="#f8f9fa"
+          >
+            <animate
+              attributeName="r"
+              values={`0;${0.1 * Math.random()};0`}
+              begin={`${Math.random()}s`}
+              dur={`${2 + 4 * Math.random()}s`}
+              repeatCount="indefinite"
+            />
+          </circle>
+        ))
+      )}
     </svg>
   );
 };
